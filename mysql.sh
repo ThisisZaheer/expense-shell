b98511b73c2c3692b305b3829dee531f652fda8a
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-15 18:33:02 +0530

    script

---
+++ mysql.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this Script with root access"
+    exit 1
+else
+    echo "You are a Super User"    
+fi
+
