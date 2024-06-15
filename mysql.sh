4809063bdd58fa4c9579800dac01ffd7eb86c028
ThisisZaheer <thisiszaheer.786@gmail.com>
2024-06-15 18:37:03 +0530

    script

--- mysql.sh
+++ mysql.sh
@@ -21,3 +21,13 @@ else
     echo "You are a Super User"    
 fi
 
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$2 is: $R Failed $N"
+    exit 1
+else
+    echo -e "$2 is : $G Success $N"
+fi
+
+}
